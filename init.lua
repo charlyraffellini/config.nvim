273600f98194405988c0d1e8b8473bdd93c78912
Carlos Raffellini <carlos.raffellini@convexin.com>
2023-05-06 22:58:54 +0100

    Adding vim visual multiline plugin

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
 require("elrafa")
-vim.opt.runtimepath:append(',~/src/vim-visual-multi/autoload/vm.vim')
+--vim.opt.runtimepath:append(',~/src/vim-visual-multi/autoload/vm.vim')
+
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+Plug('mg979/vim-visual-multi')
+
+vim.call('plug#end')
+
+
 print("Hello from elrafa")
