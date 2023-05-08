af8808d0feaac58f9f4a44e289d615ee7db0e0a4
Carlos Raffellini <carlos.raffellini@convexin.com>
2023-05-08 12:35:39 +0100

    Remap bufferline next and prev buffer command

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,5 +1,6 @@
 require("bufferline").setup({
     options = {
+        numbers = "buffer_id",
         buffer_close_icon = "x",
         close_command = "bdelete %d",
         close_icon = "X",
@@ -12,8 +13,22 @@ require("bufferline").setup({
         offsets = { { filetype = "NvimTree", text = "EXPLORER", text_align = "center" } },
         right_mouse_command = "bdelete! %d",
         right_trunc_marker = "R",
-        show_close_icon = false,
+        show_close_icon = true,
         show_tab_indicators = true,
+        get_element_icon = function(element)
+            -- element consists of {filetype: string, path: string, extension: string, directory: string}
+            -- This can be used to change how bufferline fetches the icon
+            -- for an element e.g. a buffer or a tab.
+            -- list of all supported icons: https://github.com/nvim-tree/nvim-web-devicons/blob/master/lua/nvim-web-devicons.lua
+            if element.extension == "jsonl" or element.filetype == "jsonl"
+                then
+                    local loaded, webdev_icons = pcall(require, "nvim-web-devicons")
+                    local fn, api = vim.fn, vim.api
+                    local icon, hl = webdeb_icons.get_icon(fn.fnamemodify(opts.path, ":t"), "json")
+                    return icon, hl
+                else do return end
+            end
+        end
     },
     highlights = {
         fill = {
@@ -58,3 +73,11 @@ require("bufferline").setup({
         },
     },
 })
+
+-- ==============================================================
+-- ========================== Remap =============================
+-- ==============================================================
+
+vim.keymap.set('n', '<S-l>', '<CMD>BufferLineCycleNext<CR>')
+vim.keymap.set('n', '<S-h>', '<CMD>BufferLineCyclePrev<CR>')
+
